a34ee9a8249af851119d8ffebae24983afc207ef
Enric Sanchez <83393045+enricsan1@users.noreply.github.com>
2023-07-25 12:13:19 +0200

    deploy and delete scripts

---
+++ delete_st2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+set -x
+helm uninstall bib-st2
+kubectl delete job bib-st2-job-st2-apikey-load --namespace car-bib
+kubectl delete ingress car-bib-st2-ingress --namespace car-bib
---
+++ deploy_st2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# the first time run:
+#helm dependency build
+
+helm install bib-st2 . --namespace car-bib --debug -f values.yaml
+kubectl apply -f car-bib-st2-ingress.yaml --namespace car-bib
+
